b85766c8a1a8a61d5fc7e83b453157f4e840a60c
lzw <378406712@qq.com>
2020-04-23 23:51:22 +0800

    refactor: 更新目录逻辑

--- controller/category.js
+++ controller/category.js
@@ -4,7 +4,7 @@ const Category = require('../models/category')
 const Essay = require('../models/essay')
 const ObjectId = require('mongodb').ObjectId
 const arrayDiffer = require('array-differ')
-
+const _ = require('loadsh')
 router.get('/get-essay', function(req, res) {
   Essay.find(req.query, function(err, docs) {
     res.send(docs)
@@ -90,22 +90,47 @@ router.get('/get-categoryDetail', function(req, res) {
     })
   }
 })
-
+// 更新目录
 router.post('/alter-category', function(req, res) {
-  const { _id, category } = req.body
+  const { _id, category, username } = req.body
   Category.findById({ _id: ObjectId(_id) }, function(err, docs) {
-    if (docs.category === category) {
-      //  目录名未改
-      Category.findByIdAndUpdate(
-        { _id: ObjectId(_id) },
-        req.body,
-        { new: true },
-        function(err, docs) {
-          res.send(docs)
+    Essay.where({
+      //查询含有该目录的文章
+      username: req.body.username
+    })
+      .where('checkCategory')
+      .in(docs.category)
+      .then(data => {
+        if (data.length !== 0) {
+          data.map(item => {
+            let index = item.checkCategory.indexOf(...docs.category)
+            if (index > -1) {
+              item.checkCategory.splice(index, 1, category)
+              // Essay.updateMany(
+              //   { username, checkCategory: docs.category },
+              //   {
+              //     $set: {
+              //       checkCategory: item.checkCategory
+              //     }
+              //   },
+              //   function(err) {}
+              // )
+              console.log(docs.category)
+            }
+            
+          })
         }
-      )
-    } else {
-    }
+      })
+      // .then(() => {
+      //   Category.findByIdAndUpdate(
+      //     { _id: ObjectId(_id) },
+      //     req.body,
+      //     { new: true },
+      //     function(err, docs) {
+      //       return res.send(docs)
+      //     }
+      //   )
+      // })
   })
 })
 module.exports = router
