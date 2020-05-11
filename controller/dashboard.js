f4e3a559303017a8eee623980374610b8963e5ba
lzw <378406712@qq.com>
2020-05-12 00:19:44 +0800

    refactor: set-todo

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -4,22 +4,23 @@ const Dashboard = require('../models/dashboard')
 const ObjectId = require('mongodb').ObjectId
 
 router.post('/set-todo', function (req, res) {
-    const {username} = req.body
-  Dashboard.findOneAndUpdate({username},req.body,function(err,docs){
-      if(!docs){
-          console.log('插入')
-        Dashboard.insertMany(req.body, function (err, docs) {
-            if (!err) {
-              res.send(docs)
-            } else {
-              console.log(err)
-            }
-          })
-      }else{
-        console.log('更新')
-        console.log(docs)
+  const { username } = req.body
+  Dashboard.findOneAndUpdate(
+    { username },
+    req.body,
+    { upsert: true, new: true },
+    function (err, docs) {
         res.send(docs)
-      }
+    }
+  )
+})
+router.get('/get-todo', function (req, res) {
+  const { username } = req.query
+  Dashboard.find({ username }, function (err, docs) {
+    if (docs.length !== 0) {
+      console.log(docs)
+      res.send(docs)
+    }
   })
 })
 
