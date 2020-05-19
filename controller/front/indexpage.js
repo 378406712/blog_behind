2808c6d0f936fc08d0ea05799f728ab044111503
lzw <378406712@qq.com>
2020-05-19 21:46:49 +0800

    refactor: get-boke-essay

--- controller/front/indexpage.js
+++ controller/front/indexpage.js
@@ -5,12 +5,27 @@ const ObjectId = require('mongodb').ObjectId
 // 获取文章
 router.get('/get-boke-essay', function (req, res) {
   const { username, _id } = req.query
-  Essay.find({ username, _id: ObjectId(_id) }, function (err, docs) {
-    if (!err) {
-      res.send(...docs)
-    } else {
-      return
-    }
+  let arr = []
+  Essay.find({ username, _id: ObjectId(_id) }).exec(function (err, now) {
+    arr.push({ now })
+    Essay.find({ _id: { $lt: ObjectId(_id) } })
+      .sort({ _id: -1 })
+      .limit(1)
+      .exec(function (err, previous) {
+        if (!err) {
+          arr.push({ previous })
+        }
+        Essay.find({ _id: { $gt: ObjectId(_id) } })
+          .sort({ _id: 1 })
+          .limit(1)
+          .exec(function (err, next) {
+            if (!err) {
+              arr.push({ next })
+              console.log(arr)
+              res.send(arr)
+            }
+          })
+      })
   })
 })
 
